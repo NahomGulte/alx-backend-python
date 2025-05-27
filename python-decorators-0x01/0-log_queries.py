faccd4cc2880789c3d002b802fff6f40a6754cef
NahomGulte <100404353+NahomGulte@users.noreply.github.com>
2025-05-27 14:27:29 +0300

    Create 0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,24 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+def log_sql_query(func):
+    def wrapper(*args, **kwargs):
+        query = args[1] if len(args) > 1 else "UNKNOWN QUERY"
+        print(f" SQL LOG: Executing -> {query}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
