32bd1c21bffb16a3276496c46a68732889387b69
NahomGulte <100404353+NahomGulte@users.noreply.github.com>
2025-05-27 15:03:57 +0300

    Update 2-transactional.py

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -20,31 +20,27 @@ def with_db_connection(func):
             if connection and connection.open:
                 connection.close()
                 print("DB connection closed.")
-    return wrapper
+                return wrapper
 
 def transactional(func):
-    def wrapper(*args, **kwargs):
-        connection = None
-            connection.begin()  
-            result = func(connection, *args, **kwargs)  
-
-            connection.commit()  
-            print(" Transaction committed.")
+    @functools.wraps(func)
+    def wrapper(connection, *args, **kwargs):
+        try:
+            connection.begin()
+            result = func(connection, *args, **kwargs)
+            connection.commit()
+            print("Transaction committed.")
             return result
-        except Exception as e:
-            if connection:
-                connection.rollback()  
-                print(" Transaction rolled back due to error:", e)
-        finally:
-            if connection and connection.open:
-                connection.close()
-                print("DB connection closed.")
+        except pymysql.MySQLError as e:
+            connection.rollback()
+            print("Transaction rolled back due to error:", e)
     return wrapper
 @with_db_connection 
 @transactional 
-def update_user_email(conn, user_id, new_email): 
-cursor = conn.cursor() 
-cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+def update_user_email(connection, user_id, new_email): 
+    with connection.cursor() as cursor:
+      cursor = connection.cursor() 
+      cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
 #### Update user's email with automatic transaction handling 
 
 update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
