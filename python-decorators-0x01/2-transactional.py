ec18ce91424fc8e977e16b222fb0c1611e4c8d9c
NahomGulte <100404353+NahomGulte@users.noreply.github.com>
2025-05-27 14:55:31 +0300

    Update 2-transactional.py

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,8 +1,45 @@
 import sqlite3 
 import functools
 import pymysql
-"""your code goes here"""
+def with_db_connection(func):
+      def wrapper(*args, **kwargs):
+        connection = None
+        try:
+            print("Opening DB connection...")
+            connection = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="M0han_12",
+                database="ALX_prodev"
+            )
+            result = func(connection, *args, **kwargs)
+            return result
+        except pymysql.MySQLError as e:
+            print("DB Error:", e)
+        finally:
+            if connection and connection.open:
+                connection.close()
+                print("DB connection closed.")
+    return wrapper
 
+def transactional(func):
+    def wrapper(*args, **kwargs):
+        connection = None
+            connection.begin()  
+            result = func(connection, *args, **kwargs)  
+
+            connection.commit()  
+            print(" Transaction committed.")
+            return result
+        except Exception as e:
+            if connection:
+                connection.rollback()  
+                print(" Transaction rolled back due to error:", e)
+        finally:
+            if connection and connection.open:
+                connection.close()
+                print("DB connection closed.")
+    return wrapper
 @with_db_connection 
 @transactional 
 def update_user_email(conn, user_id, new_email): 
