2f81b775023984fe20510a84cc33e2c82d6ec699
Mohanneezy <lilnahublacksmith@gmail.com>
2025-05-29 21:32:30 +0300

    getting the second job done

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import AbstractUser 
+import uuid
 from django.db import models
 
 # Create your models here.
 
-class Chats(models.Model):
+class Chats(AbstractUser):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email = models.EmailField(unique=True)
     user = models.CharField()
     conversattion = models.CharField(max_length=100)
     message = models.CharField()
