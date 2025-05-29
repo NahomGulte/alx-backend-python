bdc8ac8c4ad23f574c8c017c9679de683a97eedd
Mohanneezy <lilnahublacksmith@gmail.com>
2025-05-29 23:48:30 +0300

    getting the second job done

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework.routers import DefaultRouter
+from rest_framework.routers import api-auth
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
