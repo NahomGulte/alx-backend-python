d22203c8506ee0fc609f30b71e0eabb259dbce93
Mohanneezy <lilnahublacksmith@gmail.com>
2025-05-29 23:07:54 +0300

    getting the second job done

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,8 +2,21 @@ from rest_framework import serializers
 from .models import User
 from .models import Conversation
 from .models import Message
+from .models import Chat
 
-class UserSerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.ModelSerializer):
+    message_id = serializers.CharField(max_length=255)
+    summary = serializers.SerializerMethodField()
     class Meta:
-        model = User
+        model = Message
         fields = '__all__'
+class ConversationSerializer(serializers.ModelSerializer):
+    message_body = serializers.CharField(max_length=255)
+    summary = serializers.SerializerMethodField()
+    class Meta:
+        model = Message
+        fields = '__all__'        
+    def validate_message(self, obj):
+        if ( message_body =! obj.conversation_id):
+            raise serializers.ValidationError("Title is too short.")
+        return value
