6f416dfd1dd0405e071751aa086f7fe319b97f16
Mohanneezy <lilnahublacksmith@gmail.com>
2025-06-06 20:42:35 +0300

    late assignmet submission for unit testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -41,22 +41,26 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload) 
         
 class TestMemoize(unittest.TestCase):
-    def test_memoize():
-        class TestClass(self):
+
+    def test_memoize(self):
+        class TestClass:
             def a_method(self):
                 return 42
 
+            @property
             @memoize
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", wraps=TestClass.a_method) as mock_a_method:
-            obj = TestClass()
-            result1 = obj.a_property  
-            result2 = obj.a_property  
+        test_instance = TestClass()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
-            mock_a_method.assert_called_once(obj)
-        
+            mock_method.assert_called_once()
+            
 if __name__ == '__main__':
     unittest.main()
