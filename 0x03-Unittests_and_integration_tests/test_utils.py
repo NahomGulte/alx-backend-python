c349516ca455f5592fab9568a788f009863ffa98
Mohanneezy <lilnahublacksmith@gmail.com>
2025-06-06 14:24:12 +0300

    late assignmet submission for unit testing

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized, parameterized_class
+
+class TestAccessNestedMap (unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
